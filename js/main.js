ff1bf4a109f1cd61c068fdb75f227a883bf31fea
Chuan Li <chuanli@MacBook-Pro-de-Chuan.local>
2022-03-27 23:59:52 +0800

    feat: update

--- js/main.js
+++ js/main.js
@@ -7,13 +7,3 @@ navClose && navClose.addEventListener('click', () => {
 navOpen && navOpen.addEventListener('click', () => {
   navMenu && navMenu.classList.add('menu__active')
 })
-const str = 'We are learning FE! Go!999'
-// 小练习 获取str的最后一位
-str[str.length - 1]
-str.slice(-1)
-// 小练习2 截取str的后三位
-str.slice(-3)
-// 小练习3 查询Go!的索引
-str.indexOf('Go!')
-// 发现：str上 Boss Ding的索引
-str.indexOf('Boss Ding') // -1
